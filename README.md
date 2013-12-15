04d09051d7467bc9df8573b0f49c26450a4b6b1c
zoncoen <zoncoen@gmail.com>
2013-12-16 05:55:37 +0900

    First commit.

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Array::PrintCols::EastAsian - It's new $module
+
+# SYNOPSIS
+
+    use Array::PrintCols::EastAsian;
+
+# DESCRIPTION
+
+Array::PrintCols::EastAsian is ...
+
+# LICENSE
+
+Copyright (C) zoncoen.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+zoncoen <zoncoen@gmail.com>
---
+++ lib/Array/PrintCols/EastAsian.pm
@@ -0,0 +1,39 @@
+package Array::PrintCols::EastAsian;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Array::PrintCols::EastAsian - It's new $module
+
+=head1 SYNOPSIS
+
+    use Array::PrintCols::EastAsian;
+
+=head1 DESCRIPTION
+
+Array::PrintCols::EastAsian is ...
+
+=head1 LICENSE
+
+Copyright (C) zoncoen.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+zoncoen E<lt>zoncoen@gmail.comE<gt>
+
+=cut
+
